a7fefebc02050d11304280e17400699ad76d3c66
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-01-31 14:49:19 -0800

    add Text-based LP protocol

---
+++ src/Microsoft.AspNetCore.Sockets.Common/TextMessageBatchFormatter.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Sockets
+{
+    public static class TextMessageBatchFormatter
+    {
+        public static IEnumerable<Message> ReadMessages(ReadOnlySpan<byte> data)
+        {
+            if (data.Length < 1 || data[0] != 'T')
+            {
+                throw new FormatException("Missing 'T' prefix in Text Message Batch.");
+            }
+
+            int cursor = 1;
+            while (cursor < data.Length)
+            {
+
+                // Parse the length
+                int length = ParseLength(data, ref cursor);
+                cursor++;
+
+                // Parse the type
+                var type = ParseType(data, ref cursor);
+                cursor++;
+
+                // Read the payload
+                var payload = ParsePayload(data, length, type, ref cursor);
+                cursor++;
+
+                yield return new Message(payload, type, endOfMessage: true);
+            }
+        }
+
+        private static PreservedBuffer ParsePayload(ReadOnlySpan<byte> data, int length, MessageType messageType, ref int cursor)
+        {
+            int start = cursor;
+
+            // We know exactly where the end is. The last byte is cursor + length
+            cursor += length;
+
+            // Verify the length and trailer
+            if (cursor >= data.Length)
+            {
+                throw new FormatException("Unexpected end-of-message while reading Payload field.");
+            }
+            if (data[cursor] != ';')
+            {
+                throw new FormatException("Payload is missing trailer character ';'.");
+            }
+
+            // Read the data into a buffer
+            var buffer = new byte[length];
+            data.Slice(start, length).CopyTo(buffer);
+
+            // If the message is binary, we need to convert from Base64
+            if (messageType == MessageType.Binary)
+            {
+                // TODO: Use System.Binary.Base64 to handle this with less allocation
+
+                // Parse the data as Base64
+                var str = Encoding.UTF8.GetString(buffer);
+                buffer = Convert.FromBase64String(str);
+            }
+
+            return ReadableBuffer.Create(buffer).Preserve();
+        }
+
+        private static MessageType ParseType(ReadOnlySpan<byte> data, ref int cursor)
+        {
+            int start = cursor;
+
+            // Scan to a ':'
+            cursor = IndexOf((byte)':', data, cursor);
+            if (cursor >= data.Length)
+            {
+                throw new FormatException("Unexpected end-of-message while reading Type field.");
+            }
+
+            if (cursor - start != 1)
+            {
+                throw new FormatException("Type field must be exactly one byte long.");
+            }
+
+            switch (data[cursor - 1])
+            {
+                case (byte)'T': return MessageType.Text;
+                case (byte)'B': return MessageType.Binary;
+                case (byte)'C': return MessageType.Close;
+                case (byte)'E': return MessageType.Error;
+                default: throw new FormatException($"Unknown Type value: '{(char)data[cursor - 1]}'.");
+            }
+        }
+
+        private static int ParseLength(ReadOnlySpan<byte> data, ref int cursor)
+        {
+            int start = cursor;
+
+            // Scan to a ':'
+            cursor = IndexOf((byte)':', data, cursor);
+            if (cursor >= data.Length)
+            {
+                throw new FormatException("Unexpected end-of-message while reading Length field.");
+            }
+
+            // Parse the length
+            int length = 0;
+            for (int i = start; i < cursor; i++)
+            {
+                if (data[i] < '0' || data[i] > '9')
+                {
+                    throw new FormatException("Invalid length.");
+                }
+                length = (length * 10) + (data[i] - '0');
+            }
+
+            return length;
+        }
+
+        private static int IndexOf(byte c, ReadOnlySpan<byte> data, int start)
+        {
+            // Scan to the end or to the matching character
+            int cursor = start;
+            for (; cursor < data.Length && data[cursor] != c; cursor++) ;
+            return cursor;
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.Sockets.Common.Tests/TextMessageBatchFormatterTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Sockets.Tests
+{
+    public class TextMessageBatchFormatterTests
+    {
+        [Theory]
+        [InlineData("T0:T:;", MessageType.Text, "")]
+        [InlineData("T3:T:ABC;", MessageType.Text, "ABC")]
+        [InlineData("T11:T:A\nR\rC\r\n;DEF;", MessageType.Text, "A\nR\rC\r\n;DEF")]
+        [InlineData("T0:C:;", MessageType.Close, "")]
+        [InlineData("T17:C:Connection Closed;", MessageType.Close, "Connection Closed")]
+        [InlineData("T0:E:;", MessageType.Error, "")]
+        [InlineData("T12:E:Server Error;", MessageType.Error, "Server Error")]
+        public void ReadSingleTextMessage(string data, MessageType messageType, string payload)
+        {
+            var buffer = Encoding.UTF8.GetBytes(data);
+            var messages = TextMessageBatchFormatter.ReadMessages(buffer).ToArray();
+
+            Assert.Equal(1, messages.Length);
+            AssertMessage(messages[0], messageType, payload);
+        }
+
+        [Theory]
+        [InlineData("T0:B:;", new byte[0])]
+        [InlineData("T8:B:q83vEg==;", new byte[] { 0xAB, 0xCD, 0xEF, 0x12 })]
+        public void ReadSingleBinaryMessage(string data, byte[] payload)
+        {
+            var buffer = Encoding.UTF8.GetBytes(data);
+            var messages = TextMessageBatchFormatter.ReadMessages(buffer).ToArray();
+
+            Assert.Equal(1, messages.Length);
+            AssertMessage(messages[0], MessageType.Binary, payload);
+        }
+
+        [Fact]
+        public void ReadMultipleMessages()
+        {
+            const string data = "T0:B:;14:T:Hello,\r\nWorld!;1:C:A;12:E:Server Error;";
+            var buffer = Encoding.UTF8.GetBytes(data);
+            var messages = TextMessageBatchFormatter.ReadMessages(buffer).ToArray();
+
+            Assert.Equal(4, messages.Length);
+            AssertMessage(messages[0], MessageType.Binary, new byte[0]);
+            AssertMessage(messages[1], MessageType.Text, "Hello,\r\nWorld!");
+            AssertMessage(messages[2], MessageType.Close, "A");
+            AssertMessage(messages[3], MessageType.Error, "Server Error");
+        }
+
+        [Theory]
+        [InlineData("", "Missing 'T' prefix in Text Message Batch.")]
+        [InlineData("ABC", "Missing 'T' prefix in Text Message Batch.")]
+        [InlineData("T1230450945", "Unexpected end-of-message while reading Length field.")]
+        [InlineData("T12ab34:", "Invalid length.")]
+        [InlineData("T1:asdf", "Unexpected end-of-message while reading Type field.")]
+        [InlineData("T1::", "Type field must be exactly one byte long.")]
+        [InlineData("T1:AB:", "Type field must be exactly one byte long.")]
+        [InlineData("T5:T:A", "Unexpected end-of-message while reading Payload field.")]
+        [InlineData("T5:T:ABCDE", "Unexpected end-of-message while reading Payload field.")]
+        [InlineData("T5:T:ABCDEF", "Payload is missing trailer character ';'.")]
+        public void InvalidMessages(string data, string message)
+        {
+            var ex = Assert.Throws<FormatException>(() => TextMessageBatchFormatter.ReadMessages(Encoding.UTF8.GetBytes(data)).ToArray());
+            Assert.Equal(message, ex.Message);
+        }
+
+        private static void AssertMessage(Message message, MessageType messageType, byte[] payload)
+        {
+            Assert.True(message.EndOfMessage);
+            Assert.Equal(messageType, message.Type);
+            Assert.Equal(payload, message.Payload.Buffer.ToArray());
+        }
+
+        private static void AssertMessage(Message message, MessageType messageType, string payload)
+        {
+            Assert.True(message.EndOfMessage);
+            Assert.Equal(messageType, message.Type);
+            Assert.Equal(payload, Encoding.UTF8.GetString(message.Payload.Buffer.ToArray()));
+        }
+    }
+}
